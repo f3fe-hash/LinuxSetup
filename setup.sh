bf3975ea74ebdfd83cfef91a1979bd917711ec00
f3fe-hash <f3fe5.unhash@gmail.com>
2025-07-18 17:36:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,10 @@ sudo apt install -y gcc g++ make cmake
 sudo apt install -y curl libssl-dev freeglut3-dev
 
 # Python
-sudo apt install python3.13
+sudo apt install python3
+
+# Switch to home directory
+cd ..
+
+# Delete LinuxSetup directory
+rm -rf LinuxSetup
